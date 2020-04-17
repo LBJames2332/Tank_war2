e24a2cf4941361e0a6033fd58ed32a5ec093c9b0
LBJames2332 <1846136263@qq.com>
2020-04-17 12:45:12 +0800

    按键响应

--- src/TankFrame.java
+++ src/TankFrame.java
@@ -1,37 +1,56 @@
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
 
 public class TankFrame extends Frame{
     int x =100;
     int y =100;
     public TankFrame(){
-        System.out.println(111);
         //constructor
         this.setSize(800,600);
         this.setResizable(false);
 
         this.setVisible(true);
+        this.addKeyListener(new TankFrameLisener());
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
+
     }
 
     public static void main(String[] args) {
-        System.out.println("动起来");
         TankFrame t = new TankFrame();
+        while (true){
+            try {
+                Thread.sleep(2000);
+                t.repaint();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     @Override
     public void paint(Graphics g) {
         //随便画
         g.fillRect(x,y,40,40);
-        x+=50;
-        y+=50;
-        System.out.println("paint");
+
+    }
+    class TankFrameLisener extends KeyAdapter{
+        //键盘监听
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            x+=1;
+            //repaint();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            //System.out.println("key released");
+        }
     }
 }
