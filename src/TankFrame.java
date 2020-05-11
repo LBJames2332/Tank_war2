9163e2159be766178824a78ab07a9f7a092756dd
LBJames2332 <1846136263@qq.com>
2020-05-11 21:43:45 +0800

    按下上下左右，tank做出相应的移动。

--- src/TankFrame.java
+++ src/TankFrame.java
@@ -1,56 +1,109 @@
+import com.sun.javafx.scene.traversal.Direction;
+
 import java.awt.*;
 import java.awt.event.*;
 
 public class TankFrame extends Frame{
     int x =100;
     int y =100;
+    Direction dir = Direction.DOWN;
+    public static final int Speed = 1;//速度
     public TankFrame(){
         //constructor
         this.setSize(800,600);
         this.setResizable(false);
 
         this.setVisible(true);
-        this.addKeyListener(new TankFrameLisener());
+        this.addKeyListener(new TankKeyLisener());
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
-
-    }
-
-    public static void main(String[] args) {
-        TankFrame t = new TankFrame();
-        while (true){
-            try {
-                Thread.sleep(2000);
-                t.repaint();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-        }
     }
 
     @Override
     public void paint(Graphics g) {
         //随便画
         g.fillRect(x,y,40,40);
+        switch (dir){
+            case LEFT:
+                x-=Speed;
+                break;
+            case RIGHT:
+                x+=Speed;
+                break;
+            case UP:
+                y-=Speed;
+                break;
+            case DOWN:
+                y+=Speed;
+                break;
+            default:
+                break;
+        }
 
     }
-    class TankFrameLisener extends KeyAdapter{
-        //键盘监听
+    class TankKeyLisener extends KeyAdapter{
+        boolean bL = false;
+        boolean bR = false;
+        boolean bU = false;
+        boolean bD = false;
+
         @Override
         public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                default:
+                    break;
 
-            x+=1;
-            //repaint();
+            }
+            setMainTankDir();
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            //System.out.println("key released");
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                default:
+                    break;
+
+            }
+            setMainTankDir();
+
+        }
+
+        public void setMainTankDir(){
+            if (bL) dir = Direction.LEFT;
+            if (bR) dir = Direction.RIGHT;
+            if (bU) dir = Direction.UP;
+            if (bD) dir = Direction.DOWN;
+            repaint();
         }
     }
 }
